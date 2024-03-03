3a79e1d45b3dea413a6d658b39c2cb655e30e79e
Sasan <l.s.arham@gmail.com>
2024-03-03 22:12:33 +0330

    [Fix] Invalidate cache after editin a person

---
+++ Application/Modules/ContactManagement/People/DomainEventHandlers/PersonEditedEvent_CacheInvalidationHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common;
+using Domain.Modules.ContactManagement.People.Events;
+using MediatR;
+
+namespace Application.Modules.ContactManagement.People.DomainEventHandlers
+{
+    public class PersonEditedEvent_CacheInvalidationHandler : INotificationHandler<PersonEditedEvent>
+    {
+        private readonly IDistributedCachProvider _distributedCachProvider;
+
+        public PersonEditedEvent_CacheInvalidationHandler(IDistributedCachProvider distributedCachProvider)
+        {
+            _distributedCachProvider = distributedCachProvider;
+        }
+
+        public async Task Handle(PersonEditedEvent notification, CancellationToken cancellationToken)
+        {
+            var cachkey = $"person-{notification.ID}";
+            await _distributedCachProvider.InvalidateAsync(cachkey, cancellationToken);
+        }
+    }
+}
