bb9952829e1dab0a5a05579bef71f68581305b75
Sasan <l.s.arham@gmail.com>
2024-03-03 22:10:24 +0330

    [Fix] Invalidate cached person after deletation

---
+++ Application/Modules/ContactManagement/People/DomainEventHandlers/PersonDeletedEvent_CachInvalidationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common;
+using Domain.Modules.ContactManagement.People.Events;
+using MediatR;
+
+namespace Application.Modules.ContactManagement.People.DomainEventHandlers
+{
+    public class PersonDeletedEvent_CachInvalidationHandler : INotificationHandler<PersonDeletedEvent>
+    {
+        private readonly IDistributedCachProvider _distributedCachProvider;
+
+        public PersonDeletedEvent_CachInvalidationHandler(IDistributedCachProvider distributedCachProvider)
+        {
+            _distributedCachProvider = distributedCachProvider;
+        }
+        public async Task Handle(PersonDeletedEvent notification, CancellationToken cancellationToken)
+        {
+            var cachkey = $"person-{notification.ID}";
+            await _distributedCachProvider.InvalidateAsync(cachkey, cancellationToken);
+        }
+    }
+}
